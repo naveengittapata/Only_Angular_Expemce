5ef1b720f7396db01eb5a43597466e5d48d3c97b
unknown <naveengittapata>
2023-06-10 14:54:16 -0500

    this is my second push

--- src/app/components/expence-get/expence-get.component.ts
+++ src/app/components/expence-get/expence-get.component.ts
@@ -1,19 +1,24 @@
 import { Component } from '@angular/core';
 import { ExpenceServiceService } from 'src/app/services/expence-service.service';
-
+// export const id: number = 123;
 @Component({
   selector: 'app-expence-get',
   templateUrl: './expence-get.component.html',
   styleUrls: ['./expence-get.component.css']
 })
+
+
 export class ExpenceGetComponent {
 
   data: any = "Test"; // Data received from the API
+  mydata: any="test2";
+  id: number = 123;
 
   constructor(private apiService:ExpenceServiceService) { }
 
   ngOnInit(): void {
     this.getData();
+    this.getdataonID();
   }
 
   getData(): void {
@@ -23,7 +28,27 @@ export class ExpenceGetComponent {
       });
 
       
-    
+  }
+  getdataonID(): void {
+    debugger
+    this.apiService.getByID(this.id).subscribe(
+      response => {
+        this.mydata = response;
+      },
+      // (error: any) => {
+      //   console.error(error);
+      // }
+    );
   }
 
+  // greet(id: number): void {
+  //   // let myNumber: number = 10;
+  //   // let id: number = 12;
+  //   id = 12;
+  // }
+
 }
+
+
+
+
--- src/app/services/expence-service.service.ts
+++ src/app/services/expence-service.service.ts
@@ -3,17 +3,30 @@ import { Injectable } from '@angular/core';
   import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { ExpenceModel } from '../models/expence-model';
+import { ExpenceGetComponent} from '../components/expence-get/expence-get.component';
+// import {id} from '../components/expence-get/expence-get.component';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ExpenceServiceService {
 
-  
+ 
+
+
   private apiUrl = 'http://localhost:8080/me/first/exp'; // Endpoint URL to Get Data
   // private apiUrll = 'http://localhost:8080/me/first/exp1';
 
+  //private apiUrlgetbyID ='http://localhost:8080/me/first/exp/${this.id}'
+  // private apiUrlgetbyID = `http://localhost:8080/me/first/exp/${this.id}`;
+
+  // id: number = 1; // Replace with the actual ID value you want to use
+// private apiUrlgetbyID = `http://localhost:8080/me/first/exp/${id}`;
+
+
+
   constructor(private _http: HttpClient) {} 
+  // constructor(private _http: HttpClient, private _con:ExpenceGetComponent) {} 
 
   getData(): Observable<any> {
       return this._http.get<any>(this.apiUrl);
@@ -27,4 +40,19 @@ export class ExpenceServiceService {
     return this._http.post<ExpenceModel>(this.apiUrl, expence);
 
   }
+
+
+  // getByID(id: number): Observable<ExpenceModel>{
+  //   debugger
+  //   return this._http.get<ExpenceModel>(this.apiUrlgetbyID);
+  // }
+
+  getByID(id: number): Observable<any> {
+    const apiUrlgetbyID = `http://localhost:8080/me/first/exp/${id}`;
+    return this._http.get(apiUrlgetbyID);
+  }
+  
+
+
+
 }
